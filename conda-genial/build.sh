2eeda2ee8e156b17db4f221808472a0c97132d0c
p-barbet <barbet.pauline@orange.fr>
2019-02-19 11:15:22 +0100

    script to package the tool in conda

---
+++ conda-genial/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GENIAL",
+    version="1.0.2",
+    author="Pauline Barbet, Arnaud Felten",
+    author_email="pauline.barbet@anses.fr, arnaud.felten@anses.fr",
+    description="GENIAL: GENes Indentification with Abricate for Lucky biologists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/p-barbet/GENIAL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+        scripts=['src/GENIAL',
+             "src/GENIALanalysis",
+             "src/GENIALresults",
+             "src/GENIALmultidb",
+             "src/GENIALupdatedbs",
+             "src/GENIALsetupdbs",
+             ],
+	data_files = [('GENIALfiles/dbs/enterotox_staph',['dbs/enterotox_staph/sequences']), ('GENIALfiles/dbs/resfinder',['dbs/resfinder/sequences']), ('GENIALfiles/dbs/vfdb',['dbs/vfdb/sequences']), ('GENIALfiles/dbs/phages',['dbs/phages/sequences']), ('GENIALfiles/',['dbs/vfdb/VFs.tsv'])],
+    include_package_data=True,
+    install_requires=['pandas',   
+                      'seaborn',
+                      'biopython',
+                      'xlrd',
+                      ], 
+    dependency_links=['https://github.com/tseemann/abricate'], 
+    zip_safe=False,
+
+
+
+)
